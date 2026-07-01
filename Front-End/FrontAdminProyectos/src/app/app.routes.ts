680f6bf751f89d497345e596bddd936958ab95a3
Brandon Deaquino <deaquinobrandon5@gmail.com>
2026-07-01 11:28:30 -0600

    Interfaz de lista de los chat y agregacion de rutas

--- Front-End/FrontAdminProyectos/src/app/app.routes.ts
+++ Front-End/FrontAdminProyectos/src/app/app.routes.ts
@@ -7,6 +7,11 @@ import { CrearProyectoComponente } from './CrearProyecto/crearproyecto';
 import { PantallaConfiguracionComponente } from './Pantalla-ConfiguracionProyecto/pantallaconfiguracion';
 import { CalendarioActividadesComponente } from './CalendarioActividades/calendario';
 import { MenuConfiguracionComponente } from './MenuConfiguracion/menuconfiguracion';
+import { ChatGlobalProyectoComponente } from './Chatglobalproyecto/chatglobalproyecto';
+import { ChatActividadComponente } from './Chatactividades/chatactividades';
+import { ChatPersonalComponente } from './Chatpersonal/chatpersonal';
+import { ListaChatComponente } from './ListaChat/listachat';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -19,5 +24,10 @@ export const routes: Routes = [
   { path: 'proyecto/:id/configuracion/:opcion', component: PantallaConfiguracionComponente },
   { path: 'integrantes', component: PantallaConfiguracionComponente },
   { path: 'calendario', component: CalendarioActividadesComponente },
-  { path: 'configuracion', component: MenuConfiguracionComponente }
+  { path: 'configuracion', component: MenuConfiguracionComponente },
+  {path: 'chatglobalproyecto', component: ChatGlobalProyectoComponente},
+  {path: 'chatactividades', component: ChatActividadComponente},
+  {path: 'chatpersonal', component:ChatPersonalComponente},
+  {path: 'listaschat', component:ListaChatComponente}
+
 ];
